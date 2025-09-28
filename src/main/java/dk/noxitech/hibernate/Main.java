b3b6bac4b99ea02901011a2e65c973345ce05607
NoXi <nf@noxitech.dk>
2025-09-28 22:21:24 +0200

    Refactor logging to use ColorUtils for colored messages and add ColorUtils class for color formatting

--- src/main/java/dk/noxitech/hibernate/Main.java
+++ src/main/java/dk/noxitech/hibernate/Main.java
@@ -1,5 +1,6 @@
 package dk.noxitech.hibernate;
 
+import dk.noxitech.hibernate.utils.ColorUtils;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -17,6 +18,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.ConsoleCommandSender;
 
 public class Main extends JavaPlugin implements Listener {
     private static class HibernateConfig {
@@ -29,6 +31,7 @@ public class Main extends JavaPlugin implements Listener {
         int playerCheckCount = 0;
     }
 
+    private static ConsoleCommandSender log;
     private HibernateConfig hibernateConfig = new HibernateConfig();
     private FileConfiguration config = getConfig();
     private static boolean isHibernating = false;
@@ -53,17 +56,17 @@ public class Main extends JavaPlugin implements Listener {
                 if (args[0].equalsIgnoreCase("reload")) {
                     reloadConfig();
                     loadConfiguration();
-                    sender.sendMessage("§a[Hibernate] Configuration reloaded!");
+                    log.sendMessage(ColorUtils.getColored("&a[Hibernate] Configuration reloaded!"));
                     return true;
                 }
                 if (args[0].equalsIgnoreCase("status")) {
-                    sender.sendMessage(String.format("§a[Hibernate] Hibernation is currently §%s%s",
+                    sender.sendMessage(String.format(ColorUtils.getColored("&a[Hibernate] Hibernation is currently &%s%s"),
                         hibernateConfig.enabled ? "a" : "c",
                         hibernateConfig.enabled ? "enabled" : "disabled"));
-                    sender.sendMessage(String.format("§a[Hibernate] Deep hibernation is §%s%s",
+                    sender.sendMessage(String.format(ColorUtils.getColored("&a[Hibernate] Deep hibernation is &%s%s"),
                         hibernateConfig.deepHibernation ? "a" : "c",
                         hibernateConfig.deepHibernation ? "enabled" : "disabled"));
-                    sender.sendMessage(String.format("§a[Hibernate] Hibernation countdown: §e%d seconds",
+                    sender.sendMessage(String.format(ColorUtils.getColored("&a[Hibernate] Hibernation countdown: &e%d seconds"),
                         hibernateConfig.hibernationCountdown));
                     
                     int onlinePlayerCount = getServer().getOnlinePlayers().size();
@@ -82,19 +85,19 @@ public class Main extends JavaPlugin implements Listener {
                             color = "e";
                         }
                     }
-                    sender.sendMessage(String.format("§a[Hibernate] The server is currently §%s%s", color, serverState));
+                    sender.sendMessage(String.format(ColorUtils.getColored("&a[Hibernate] The server is currently &%s%s"), color, serverState));
                     return true;
                 }
                 if (args[0].equalsIgnoreCase("wake")) {
                     if (countdownActive) {
                         countdownActive = false;
                         countdownTimer = 0;
-                        sender.sendMessage("§a[Hibernate] Hibernation countdown cancelled!");
+                        log.sendMessage(ColorUtils.getColored("&a[Hibernate] Hibernation countdown cancelled!"));
                     } else if (isDeepHibernating || isHibernating) {
                         wakeUpServer();
-                        sender.sendMessage("§a[Hibernate] Server has been manually awakened!");
+                        log.sendMessage(ColorUtils.getColored("&a[Hibernate] Server has been manually awakened!"));
                     } else {
-                        sender.sendMessage("§c[Hibernate] Server is already awake and no countdown is active!");
+                        log.sendMessage(ColorUtils.getColored("&c[Hibernate] Server is already awake and no countdown is active!"));
                     }
                     return true;
                 }
@@ -105,14 +108,14 @@ public class Main extends JavaPlugin implements Listener {
                         } else {
                             unloadChunksAndCleanMemory();
                         }
-                        sender.sendMessage("§a[Hibernate] Server has been manually put to sleep!");
+                        log.sendMessage(ColorUtils.getColored("&a[Hibernate] Server has been manually put to sleep!"));
                     } else {
-                        sender.sendMessage("§c[Hibernate] Server is already hibernating!");
+                        log.sendMessage(ColorUtils.getColored("&c[Hibernate] Server is already hibernating!"));
                     }
                     return true;
                 }
             } else {
-                sender.sendMessage("§c[Hibernate] Usage: /hibernate <reload|status|wake|sleep>");
+                log.sendMessage(ColorUtils.getColored("&c[Hibernate] Usage: /hibernate <reload|status|wake|sleep>"));
                 return true;
             }
         }
@@ -121,6 +124,7 @@ public class Main extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
+        log = Bukkit.getConsoleSender();
         saveDefaultConfig();
         loadConfiguration();
 
@@ -129,15 +133,15 @@ public class Main extends JavaPlugin implements Listener {
         if (foundBlacklistedPlugins.isEmpty()) {
             startHibernationTask();
             isHibernating = true;
-            getLogger().info("Hibernate system started successfully!");
+            log.sendMessage(ColorUtils.getColored("&a[Hibernate] Hibernate system started successfully!"));
             if (hibernateConfig.deepHibernation) {
-                getLogger().info("Deep hibernation mode is ENABLED - Use with caution!");
+                log.sendMessage(ColorUtils.getColored("&6[Hibernate] Deep hibernation mode is ENABLED - Use with caution!"));
             }
         } else {
-            getLogger().warning("Blacklisted plugins were found (plugins that will stop working if Hibernate is enabled). " +
+            log.sendMessage(ColorUtils.getColored("&c[Hibernate] Blacklisted plugins were found (plugins that will stop working if Hibernate is enabled). " +
                 "Standard hibernation will be disabled, but Hibernate will still try to unload chunks. " +
-                "(check the Hibernate config and edit the blacklist if you want to force enable Hibernate)");
-            getLogger().warning("Blacklisted plugins: " + String.join(", ", foundBlacklistedPlugins));
+                "(check the Hibernate config and edit the blacklist if you want to force enable Hibernate)"));
+            log.sendMessage(ColorUtils.getColored("&4[Hibernate] Blacklisted plugins: " + String.join(", ", foundBlacklistedPlugins)));
         }
 
         if (hibernateConfig.unloadChunks) {
@@ -219,7 +223,7 @@ public class Main extends JavaPlugin implements Listener {
                 if (onlineCount > 0 && countdownActive) {
                     countdownActive = false;
                     countdownTimer = 0;
-                    getLogger().info("§6[Hibernate] Hibernation countdown cancelled - players joined!");
+                    log.sendMessage(ColorUtils.getColored("&6[Hibernate] Hibernation countdown cancelled - players joined!"));
                     return;
                 }
 
@@ -232,16 +236,16 @@ public class Main extends JavaPlugin implements Listener {
                 if (onlineCount == 0 && hibernateConfig.enabled) {
                     if (hibernateConfig.playerCheckCount != 0) {
                         String hibernationType = hibernateConfig.deepHibernation ? "Deep Hibernation" : "Hibernation";
-                        getLogger().info("§6[Hibernate] Saving worlds before " + hibernationType);
+                        log.sendMessage(ColorUtils.getColored("&6[Hibernate] Saving worlds before " + hibernationType));
                         for (World world : server.getWorlds()) {
                             world.save();
                         }
-                        getLogger().info("§6[Hibernate] Worlds saved!");
+                        log.sendMessage(ColorUtils.getColored("&6[Hibernate] Worlds saved!"));
                         hibernateConfig.playerCheckCount = 0;
 
                         countdownActive = true;
                         countdownTimer = hibernateConfig.hibernationCountdown;
-                        getLogger().info("§6[Hibernate] Starting hibernation countdown: " + countdownTimer + " seconds");
+                        log.sendMessage(ColorUtils.getColored("&6[Hibernate] Starting hibernation countdown: " + countdownTimer + " seconds"));
                         return;
                     }
 
@@ -249,13 +253,13 @@ public class Main extends JavaPlugin implements Listener {
                         countdownTimer--;
 
                         if (countdownTimer == 20 || countdownTimer == 10 || countdownTimer <= 5 && countdownTimer > 0) {
-                            Bukkit.broadcastMessage("§6[Hibernate] Server will hibernate in " + countdownTimer + " seconds...");
+                            Bukkit.broadcastMessage(ColorUtils.getColored("&6[Hibernate] Server will hibernate in " + countdownTimer + " seconds..."));
                         }
 
                         if (countdownTimer <= 0) {
                             countdownActive = false;
-                            getLogger().info("§6[Hibernate] Countdown finished - starting hibernation!");
-                            
+                            log.sendMessage(ColorUtils.getColored("&6[Hibernate] Hibernation countdown completed - No players online, entering hibernation mode"));
+
                             if (hibernateConfig.deepHibernation && !isDeepHibernating) {
                                 deepHibernation();
                             } else if (!hibernateConfig.deepHibernation) {
@@ -297,7 +301,7 @@ public class Main extends JavaPlugin implements Listener {
     private void deepHibernation() {
         if (getServer().getOnlinePlayers().size() == 0 && !isDeepHibernating) {
             isDeepHibernating = true;
-            getLogger().info("=== ENTERING DEEP HIBERNATION MODE ===");
+            log.sendMessage(ColorUtils.getColored("&6[Hibernate] === ENTERING DEEP HIBERNATION MODE ==="));
 
             unloadAllChunksAggressively();
 
@@ -320,7 +324,7 @@ public class Main extends JavaPlugin implements Listener {
                 Thread.currentThread().interrupt();
             }
 
-            getLogger().info("Deep hibernation cycle completed");
+            log.sendMessage(ColorUtils.getColored("&a[Hibernate] Deep hibernation cycle completed"));
         }
     }
 
@@ -338,7 +342,7 @@ public class Main extends JavaPlugin implements Listener {
             }
         }
 
-        getLogger().info("Deep hibernation: Aggressively unloaded " + totalUnloaded + " chunks");
+        log.sendMessage(ColorUtils.getColored("&a[Hibernate] Deep hibernation: Aggressively unloaded " + totalUnloaded + " chunks"));
     }
 
     private void freezeServerProcesses() {
@@ -349,16 +353,16 @@ public class Main extends JavaPlugin implements Listener {
                 world.setAutoSave(false);
             }
 
-            getLogger().info("Server processes frozen for deep hibernation");
+            log.sendMessage(ColorUtils.getColored("&a[Hibernate] Server processes frozen for deep hibernation"));
 
         } catch (Exception e) {
-            getLogger().warning("Could not freeze all processes: " + e.getMessage());
+            log.sendMessage(ColorUtils.getColored("&c[Hibernate] Could not freeze all processes: " + e.getMessage()));
         }
     }
 
     private void wakeUpServer() {
         if (isDeepHibernating) {
-            getLogger().info("=== WAKING UP FROM DEEP HIBERNATION ===");
+            log.sendMessage(ColorUtils.getColored("&a[Hibernate] Waking up from deep hibernation..."));
 
             for (World world : Bukkit.getWorlds()) {
                 world.setSpawnFlags(true, true);
@@ -369,15 +373,15 @@ public class Main extends JavaPlugin implements Listener {
             System.gc();
 
             isDeepHibernating = false;
-            getLogger().info("Server awakened from deep hibernation!");
+            log.sendMessage(ColorUtils.getColored("&a[Hibernate] Server awakened from deep hibernation!"));
         } else if (isHibernating) {
-            getLogger().info("Server awakened from normal hibernation");
+            log.sendMessage(ColorUtils.getColored("&a[Hibernate] Server awakened from normal hibernation"));
         }
 
         if (countdownActive) {
             countdownActive = false;
             countdownTimer = 0;
-            getLogger().info("§6[Hibernate] Hibernation countdown cancelled due to server wake up");
+            log.sendMessage(ColorUtils.getColored("&6[Hibernate] Hibernation countdown cancelled due to server wake up"));
         }
 
         isHibernating = false;
@@ -400,7 +404,7 @@ public class Main extends JavaPlugin implements Listener {
         }
 
         if (unloadedCount > 0) {
-            getLogger().info(String.format("Unloaded %d chunks", unloadedCount));
+            log.sendMessage(ColorUtils.getColored(String.format("&a[Hibernate] Unloaded %d chunks", unloadedCount)));
             performGarbageCollection();
         }
     }
@@ -412,7 +416,7 @@ public class Main extends JavaPlugin implements Listener {
         long memoryFreed = (memoryAfter - memoryBefore) / 1024L / 1024L;
 
         if (memoryFreed > 0L) {
-            getLogger().info(String.format("%d MB memory freed using Java garbage collector", memoryFreed));
+            log.sendMessage(ColorUtils.getColored(String.format("&a[Hibernate] %d MB memory freed using Java garbage collector", memoryFreed)));
         }
     }
 }
